3f85310c70daf74a613bf2a96a0516e401997964
Toshiyuki HINA <thina@yourinventit.com>
2016-04-20 11:23:01 +0900

    Add babel-plugin-add-module-exports

--- app/assets/javascripts/components/hello.js.jsx
+++ app/assets/javascripts/components/hello.js.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default class Hello extends React.Component {
   render() {
-    return (
-      <div>Hello React</div>
-    );
+    return (<div>Hello React</div>);
   }
 }
